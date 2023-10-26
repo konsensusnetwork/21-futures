6ed5b1896d7d6ef24b42be9b770d1152223d92a2
Thibaut <thibaut@konsensus.network>
2023-10-26 02:08:32 +0200

    Updates to main landingpage

--- src/data/settings.js
+++ src/data/settings.js
@@ -1,7 +1,7 @@
 export default {
   title: `21 Futures`,
   description: `21 Futures is a bitcoin anthology book, published by Konsensus Network. It is a collection of 21 short stories, each set in a different possible future, all of which explore the societal impact of bitcoin.`,
-  url: `https://21-futures.pages.dev/`, // No trailing slash!
+  url: `https://21futures.com`, // No trailing slash!
   // JSON LD
   name: `21 Futures`,
   // Twitter URL
