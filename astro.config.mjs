9da284e1c1824c2733e49878d3d51a602e3d9abb
Thibaut <thibaut@konsensus.network>
2023-11-13 16:36:23 +0100

    Update URL in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://21-futures.com/",
+  site: "https://21futures.com/",
   integrations: [embeds(), mdx(), tailwind(), react()]
 });
